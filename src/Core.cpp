1ecd0e5a1f01a2f23937a1d507b1e5320bb2100e
Nahuel Rocchetti <Nahuel3d@gmail.com>
2025-02-09 02:17:38 -0300

    crappy a8 workaround

--- src/Core.cpp
+++ src/Core.cpp
@@ -10,6 +10,61 @@
 #include "MinHook.h"
 #include "scan.h"
 #include "LuaExtensions.h"
+#include "d3d9.h"
+
+typedef IDirect3D9* (__stdcall* DIRECT3DCREATE9)(UINT);
+typedef HRESULT(WINAPI* tCheckDeviceFormat)(
+	IDirect3D9* pThis,
+	UINT Adapter,
+	D3DDEVTYPE DeviceType,
+	D3DFORMAT AdapterFormat,
+	DWORD Usage,
+	D3DRESOURCETYPE RType,
+	D3DFORMAT CheckFormat
+	);
+
+DIRECT3DCREATE9 fpDirect3DCreate9 = NULL;
+tCheckDeviceFormat fpCheckDeviceFormat = nullptr;
+
+HRESULT WINAPI hkCheckDeviceFormat(
+	IDirect3D9* pThis,
+	UINT Adapter,
+	D3DDEVTYPE DeviceType,
+	D3DFORMAT AdapterFormat,
+	DWORD Usage,
+	D3DRESOURCETYPE RType,
+	D3DFORMAT CheckFormat
+) {
+	printf("[Hooked] CheckDeviceFormat called\n");
+	printf("Adapter: %d, DeviceType: %d, AdapterFormat: %d, Usage: 0x%X, RType: %d, CheckFormat: %d\n",
+		Adapter, DeviceType, AdapterFormat, Usage, RType, CheckFormat);
+
+	// Call the original function
+	HRESULT res = fpCheckDeviceFormat(pThis, Adapter, DeviceType, AdapterFormat, Usage, RType, CheckFormat);
+	if (CheckFormat == D3DFMT_A8) {
+		return D3DERR_NOTAVAILABLE;
+	}
+	return res;
+}
+
+IDirect3D9* __stdcall DetourDirect3DCreate9(UINT sdkVersion) {
+	IDirect3D9* device = fpDirect3DCreate9(sdkVersion);
+	Log("CREATED D3D!\n");
+	void** vtable = *(void***)device;
+	Log("Hooking %p\n", vtable[10]);
+	if (MH_CreateHook(vtable[10], &hkCheckDeviceFormat,
+		reinterpret_cast<LPVOID*>(&fpCheckDeviceFormat)) != MH_OK)
+	{
+		return device;
+	}
+
+	if (MH_EnableHook(vtable[10]) != MH_OK)
+	{
+		return device;
+	}
+
+	return device;
+}
 
 typedef unsigned int(__thiscall* RANDOMUINT32UNIFORM)(TS2::cRZRandom*);
 typedef LPWSTR(WINAPI* GETCOMMANDLINEW)();
@@ -156,6 +211,27 @@ bool Core::Initialize() {
 			return false;
 		}
 		if (!LuaExtensions::Initialize()) return false;
+	}
+
+	HMODULE hD3D9 = GetModuleHandleA("d3d9.dll");
+	if (!hD3D9) {
+		return false;
+	}
+
+	void* pDirect3DCreate9 = GetProcAddress(hD3D9, "Direct3DCreate9");
+	if (!pDirect3DCreate9) {
+		return false;
+	}
+
+	if (MH_CreateHook(pDirect3DCreate9, &DetourDirect3DCreate9,
+		reinterpret_cast<LPVOID*>(&fpDirect3DCreate9)) != MH_OK)
+	{
+		return false;
+	}
+
+	if (MH_EnableHook(pDirect3DCreate9) != MH_OK)
+	{
+		return false;
 	}
 
 	return true;
