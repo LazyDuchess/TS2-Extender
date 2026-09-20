dd4207584349d705815e66574ad9a02146cce067
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2026-09-20 16:57:51 -0300

    add ability to grab char ptr from cRZString

--- include/ts2/cRZString.h
+++ include/ts2/cRZString.h
@@ -2,4 +2,7 @@
 class cRZString {
 public:
 	void FromChar(const char* str);
+	const char* GetString();
+private:
+	bool IsEmbedded();
 };
--- src/ts2/cRZString.cpp
+++ src/ts2/cRZString.cpp
@@ -4,3 +4,13 @@
 void cRZString::FromChar(const char* str) {
 	((void(__thiscall*)(cRZString*, const char*))Addresses::CRZStringFromChar)(this, str);
 }
+
+const char* cRZString::GetString() {
+	if (IsEmbedded())
+		return (const char*)(this + 0x4);
+	return *(const char**)(this + 0x4);
+}
+
+bool cRZString::IsEmbedded() {
+	return (*(char*)(this + 0x18)) == 0x0F;
+}
