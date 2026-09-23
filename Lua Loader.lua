8f2a7bfddf8d4aa3eb1b43ff0f20f3df2dcd3bd1
Nahuel Rocchetti <42678262+LazyDuchess@users.noreply.github.com>
2026-09-23 04:38:50 -0300

    Lua loader: Run extender script first

--- Lua Loader.lua
+++ Lua Loader.lua
@@ -3,16 +3,37 @@ print("Lua Loader Initializing.")
 
 local luaFiles = GetFilesInDirectory(luaDir, ".lua")
 
-for k, v in ipairs(luaFiles) do
-	print("Executing " .. v)
-	local script = ReadFile(v)
+local function IsPriorityScript(path)
+	if string.len(path) < 16 then
+		return false
+	end
+	path = string.lower(path)
+	local sub = string.sub(path, -16)
+	return sub == "/ts2extender.lua" or sub == "\\ts2extender.lua"
+end
+
+local function RunScriptFile(path)
+	print("Executing " .. path)
+	local script = ReadFile(path)
 
 	local ok, err = pcall(function(fn)
 		loadstring(fn)()
 	end, script)
 
 	if not ok then
-		print("Error executing " .. v)
+		print("Error executing " .. path)
 		print(err)
+	end
+end
+
+for k, v in ipairs(luaFiles) do
+	if IsPriorityScript(v) then
+		RunScriptFile(v)
+	end
+end
+
+for k, v in ipairs(luaFiles) do
+	if not IsPriorityScript(v) then
+		RunScriptFile(v)
 	end
 end
